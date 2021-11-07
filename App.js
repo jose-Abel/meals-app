e7a4ad8999d9faa6ed9bd798767608bd572f2415
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-07 11:08:42 -0400

    Create the MealsNavigator

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import AppLoading from 'expo-app-loading';
+import MealsNavigator from './navigation/MealsNavigator';
 
 const fetchFonts = () => {
   return Font.loadAsync({
     'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
     'opens-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  })
+  });
 }
 
 export default function App() {
@@ -23,9 +24,5 @@ export default function App() {
     )
   }
 
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />
 }
