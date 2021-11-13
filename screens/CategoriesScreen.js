533a93714459b98cff850863076cba2fa7b782e8
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-13 16:27:03 -0400

    Create a model for the categories and dummy data to render it on the CategoriesScreen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>
+        {itemData.item.title}
+      </Text>
+    </View>
+  )
+}
 
 const CategoriesScreen = props => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button 
-        title="Go to Meals"
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'CategoryMeals'
-          });
-        }}
-      />
-    </View>
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
   )
 }
 
@@ -22,6 +24,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 
