62ecaa4a1c814eff067c2d71354ce2893930fa79
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-25 21:58:48 -0400

    Add the react-navigation-header-buttons package to add a Star on the MealDetailScreen so that the user can save the meal as favorie

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -25,6 +27,18 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
   return {
     headerTitle: selectedMeal.title,
+    headerRight: () => 
+    <HeaderButtons  
+      HeaderButtonComponent={CustomHeaderButton}
+    >
+      <Item 
+        title="Favorite"
+        iconName="ios-star"
+        onPress={()=> {
+          console.log("Mark as favorite");
+        }}
+      />
+    </HeaderButtons>
   }
 }
 
