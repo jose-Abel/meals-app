f52c35c8b44ce73d1de3fbdf8b69e16546dca631
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-27 21:24:50 -0400

    Add the react-navigation-tabs package to add the tabs features to the app

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -10,9 +10,9 @@ const CategoryGridTile = props => {
   return (
     <View style={styles.gridItem}>
       <TouchableCmpt
-        style={{flex: 1}}
+        style={{ flex: 1 }}
         onPress={props.onSelect} >
-        <View style={{...styles.container, ...{backgroundColor: props.color}}}>
+        <View style={{ ...styles.container, ...{ backgroundColor: props.color } }}>
 
           <Text style={styles.title} numberOfLines={2}>
             {props.title}
@@ -29,16 +29,16 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden'
+    elevation: 3,
+    overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
   },
   container: {
     flex: 1,
     borderRadius: 10,
     shadowColor: 'black',
     shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2},
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from "react-navigation-tabs";
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
 import Colors from '../constants/Colors';
+import FavoritesScreen from '../screens/FavoritesScreen';
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
@@ -21,4 +23,9 @@ const MealsNavigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+  Meals: MealsNavigator,
+  Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
