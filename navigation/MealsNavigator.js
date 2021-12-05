63646b6be6a0fee0ff4c41fe8372143ce8aea45f
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2021-12-05 11:40:41 -0400

    Give some style to the drawer navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -66,11 +66,27 @@ const MealsFavTabNavigator = Platform.OS === 'android' ? createMaterialBottomTab
 
   const FiltersNavigator = createStackNavigator({
     Filters: FiltersScreen
-  })
+  }, {
+    // navigationOptions: {
+    //   drawerLabel: "Filters!"
+    // },
+    defaultNavigationOptions: defaultStackNavOptions
+  });
 
 const MainNavigator = createDrawerNavigator({
-  MealsFavs: MealsFavTabNavigator,
+  MealsFavs: {
+    screen: MealsFavTabNavigator,
+    navigationOptions: {
+    drawerLabel: "Meals"
+  }},
   Filters: FiltersNavigator
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: 'open-sans-bold'
+    }
+  }
 });
 
 export default createAppContainer(MainNavigator);
